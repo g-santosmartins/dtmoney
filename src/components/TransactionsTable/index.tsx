314fe0f89ffdc1228ccd0cb0b34eee2826b72eff
g-santosmartins <g.santosmartins@outlook.com>
2021-10-04 17:26:40 -0300

    Terminando intregração com axios e miragejs com fake API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react'
+import { api } from '../../services/api'
 import { Container } from './styles'
 
 
 export function TransactionsTable() {
+
+
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, [])
+
+
   return (
     <Container>
       <table>
@@ -16,12 +26,12 @@ export function TransactionsTable() {
 
 
         <tbody>
+
           <tr>
             <td>Desenvolvimento de website</td>
             <td className="deposit">R$12.000</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
-            
           </tr>
 
 
